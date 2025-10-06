df81410b11c5d4afbd8d84684a8d7a62efb2d6da
tg-prplx <automatick07@gmail.com>
2025-10-06 11:18:39 +0100

    Hangman 3-rd stage

--- src/Hangman/hangman.java
+++ src/Hangman/hangman.java
@@ -1,14 +1,15 @@
 package Hangman;
 
-import java.util.Scanner;
+import java.util.*;
 
 public class hangman {
     public static void main(String[] args) {
         System.out.println("HANGMAN");
         Scanner sc = new Scanner(System.in);
+        List<String> words = Arrays.asList("python", "java", "javascript", "kotlin");
+        String answer = words.get(new Random().nextInt(words.size()));
         System.out.print("Guess the word: ");
         String guess = sc.nextLine();
-        String answer = "java";
         if (answer.equals(guess)) {
             System.out.println("You survived!");
         } else {
