fd871f9848db4962f35828fc3186359554dcbadb
tg-prplx <automatick07@gmail.com>
2025-10-06 11:09:53 +0100

    ChatBot 2-nd stage

--- src/ChatBot/ChatBot.java
+++ src/ChatBot/ChatBot.java
@@ -1,6 +1,12 @@
+import java.util.Scanner;
+
 public class ChatBot {
     public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
         System.out.println("Hello! My name is Aid.");
         System.out.println("I was created in 2025.");
+        System.out.println("Please, remind me your name.");
+        String name = scanner.nextLine();
+        System.out.println("What a great name you have, " + name + "!");
     }
 }
