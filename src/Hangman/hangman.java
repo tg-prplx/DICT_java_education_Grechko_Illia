ac258851e007e088a93ae9e913b5eee510870812
tg-prplx <automatick07@gmail.com>
2025-10-06 11:18:40 +0100

    Hangman 5-th stage

--- src/Hangman/hangman.java
+++ src/Hangman/hangman.java
@@ -8,17 +8,27 @@ public class hangman {
         Scanner sc = new Scanner(System.in);
         List<String> words = Arrays.asList("python", "java", "javascript", "kotlin");
         String answer = words.get(new Random().nextInt(words.size()));
-        StringBuilder hint = new StringBuilder();
-        for (int i = 0; i < answer.length(); i++) {
-            if (i < 2) hint.append(answer.charAt(i));
-            else hint.append('-');
+        char[] mask = new char[answer.length()];
+        Arrays.fill(mask, '-');
+        for (int tries = 0; tries < 8; tries++) {
+            System.out.println();
+            System.out.println(new String(mask));
+            System.out.print("Input a letter: ");
+            String s = sc.nextLine();
+            if (s.isEmpty()) continue;
+            char ch = s.charAt(0);
+            boolean opened = false;
+            for (int i = 0; i < answer.length(); i++) {
+                if (answer.charAt(i) == ch) {
+                    if (mask[i] != ch) { mask[i] = ch; opened = true; }
+                }
+            }
+            if (!opened) {
+                System.out.println("That letter doesn't appear in the word");
+            }
         }
-        System.out.print("Guess the word " + hint + ": ");
-        String guess = sc.nextLine();
-        if (answer.equals(guess)) {
-            System.out.println("You survived!");
-        } else {
-            System.out.println("You lost!");
-        }
+        System.out.println();
+        System.out.println("Thanks for playing!");
+        System.out.println("We'll see how well you did in the next stage");
     }
 }
