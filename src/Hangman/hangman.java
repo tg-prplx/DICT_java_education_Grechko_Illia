d4edfede0677415539093a11063704173996762f
tg-prplx <automatick07@gmail.com>
2025-10-06 11:18:40 +0100

    Hangman 7-th stage

--- src/Hangman/hangman.java
+++ src/Hangman/hangman.java
@@ -5,19 +5,36 @@ import java.util.*;
 public class hangman {
     public static void main(String[] args) {
         System.out.println("HANGMAN");
+        play();
+    }
+
+    static void play() {
         Scanner sc = new Scanner(System.in);
         List<String> words = Arrays.asList("python", "java", "javascript", "kotlin");
         String answer = words.get(new Random().nextInt(words.size()));
         char[] mask = new char[answer.length()];
         Arrays.fill(mask, '-');
         int lives = 8;
+        Set<Character> guessed = new HashSet<>();
         while (lives > 0 && !(new String(mask)).equals(answer)) {
             System.out.println();
             System.out.println(new String(mask));
             System.out.print("Input a letter: ");
             String s = sc.nextLine();
-            if (s.isEmpty()) continue;
+            if (s.length() != 1) {
+                System.out.println("You should input a single letter");
+                continue;
+            }
             char ch = s.charAt(0);
+            if (ch < 'a' || ch > 'z') {
+                System.out.println("Please enter a lowercase English letter");
+                continue;
+            }
+            if (guessed.contains(ch)) {
+                System.out.println("You've already guessed this letter");
+                continue;
+            }
+            guessed.add(ch);
             boolean improved = false;
             boolean inWord = false;
             for (int i = 0; i < answer.length(); i++) {
@@ -31,11 +48,10 @@ public class hangman {
                 lives--;
             } else if (!improved) {
                 System.out.println("No improvements");
-                lives--;
             }
         }
         if ((new String(mask)).equals(answer)) {
-            System.out.println("You guessed the word!");
+            System.out.println("You guessed the word " + answer + "!");
             System.out.println("You survived!");
         } else {
             System.out.println("You lost!");
