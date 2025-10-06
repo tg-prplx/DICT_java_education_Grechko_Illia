52a4da34b076f78f43bf86c2234c5cdddc4fed85
tg-prplx <automatick07@gmail.com>
2025-10-06 11:09:52 +0100

    ChatBot 1-st stage

---
+++ src/ChatBot/ChatBot.java
@@ -0,0 +1,6 @@
+public class ChatBot {
+    public static void main(String[] args) {
+        System.out.println("Hello! My name is Aid.");
+        System.out.println("I was created in 2025.");
+    }
+}
