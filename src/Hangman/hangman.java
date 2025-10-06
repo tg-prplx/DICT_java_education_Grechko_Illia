d41634ccdd44ce07015d627c63e141cb534d056e
tg-prplx <automatick07@gmail.com>
2025-10-06 11:18:39 +0100

    Hangman 2-nd stage

--- src/Hangman/hangman.java
+++ src/Hangman/hangman.java
@@ -1,8 +1,18 @@
 package Hangman;
 
+import java.util.Scanner;
+
 public class hangman {
     public static void main(String[] args) {
         System.out.println("HANGMAN");
-        System.out.println("The game will be available soon.");
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Guess the word: ");
+        String guess = sc.nextLine();
+        String answer = "java";
+        if (answer.equals(guess)) {
+            System.out.println("You survived!");
+        } else {
+            System.out.println("You lost!");
+        }
     }
 }
